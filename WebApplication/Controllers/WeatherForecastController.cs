da5bd63b2e7fa7e460a77ebe559f64747ad4e261
Suhas Parameshwara <suhasparameshwara@gmail.com>
2019-12-15 15:38:24 +0530

    ASP.NET Core Web API demo

---
+++ WebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+						"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+				};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<WeatherForecast> Get()
+		{
+			var rng = new Random();
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = rng.Next(-20, 55),
+				Summary = Summaries[rng.Next(Summaries.Length)]
+			})
+			.ToArray();
+		}
+
+		[HttpGet("{cityName}")]
+		public string GetWeatherByCountryName(string cityName)
+		{
+			if (cityName.Equals("Coimbatore", StringComparison.OrdinalIgnoreCase))
+			{
+				return "Climate is Cool";
+			}
+			return cityName.Equals("Pollachi", StringComparison.OrdinalIgnoreCase) 
+				? "Expecting heavy rains" 
+				: "Invalid Input";
+		}
+	}
+}
